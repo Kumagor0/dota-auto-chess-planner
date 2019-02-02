262ee992a445e8bb7f0f503fb30f3f95c23f7606
Gleb Gladkiy <gleba@yandex.ru>
2019-02-02 23:53:59 +0300

    fix: displaying heroes in 2 columns

--- src/HeroesList.tsx
+++ src/HeroesList.tsx
@@ -3,35 +3,91 @@ import * as React from 'react';
 import { heroes, HeroType, getFeaturesCount } from './heroes';
 import { colors } from './colors';
 
-type State = {
-  pickedHeroes: Array<HeroType['name']>;
+type PickedHeroesType = Array<HeroType['name']>;
+
+type HeroesListState = {
+  pickedHeroes: PickedHeroesType;
 };
 
 const Feature = ({ name }: { name: string }) => (
   <span style={{ color: colors.features[name] }}>{name}</span>
 );
 
-export class HeroesList extends React.Component<{}, State> {
-  state: State = {
-    pickedHeroes: [],
-  };
-
+class HeroesTable extends React.Component<
+  {
+    heroes: Array<HeroType>;
+    pickedHeroes: PickedHeroesType;
+    onPickedHeroesChange: (newPickedHeroes: PickedHeroesType) => void;
+  },
+  {}
+> {
   handleHeroClick = (heroName: HeroType['name']) => {
-    const index = this.state.pickedHeroes.indexOf(heroName);
+    const index = this.props.pickedHeroes.indexOf(heroName);
 
     if (index === -1) {
-      this.setState({
-        pickedHeroes: [...this.state.pickedHeroes, heroName],
-      });
+      this.props.onPickedHeroesChange([...this.props.pickedHeroes, heroName]);
     } else {
-      const newPickedHeroes = [...this.state.pickedHeroes];
+      const newPickedHeroes = [...this.props.pickedHeroes];
 
       newPickedHeroes.splice(index, 1);
 
-      this.setState({
-        pickedHeroes: newPickedHeroes,
-      });
+      this.props.onPickedHeroesChange(newPickedHeroes);
     }
+  };
+
+  render() {
+    const { heroes, pickedHeroes } = this.props;
+
+    return (
+      <table>
+        <thead style={{ color: 'White' }}>
+          <tr>
+            <th>Name</th>
+            <th>Species</th>
+            <th>Class</th>
+            {/* <th>Ability</th> */}
+            <th>Cost</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {heroes.map(({ name, species, className, cost }) => (
+            <tr
+              key={name}
+              onClick={() => {
+                this.handleHeroClick(name);
+              }}
+              style={{
+                backgroundColor:
+                  pickedHeroes.indexOf(name) === -1 ? 'Black' : 'DarkSlateGrey',
+              }}
+            >
+              <td style={{ color: colors.cost[cost], fontWeight: 'bold' }}>
+                {name}
+              </td>
+              <td>
+                {species.map(speciesName => (
+                  <React.Fragment key={speciesName}>
+                    <Feature name={speciesName} />{' '}
+                  </React.Fragment>
+                ))}
+              </td>
+              <td>
+                <Feature name={className} />
+              </td>
+              {/* <td> </td> */}
+              <td style={{ color: 'White' }}>{cost}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+export class HeroesList extends React.Component<{}, HeroesListState> {
+  state: HeroesListState = {
+    pickedHeroes: [],
   };
 
   render() {
@@ -42,59 +98,28 @@ export class HeroesList extends React.Component<{}, State> {
         <h1 style={{ color: 'White' }}>
           Click on heroes to add/remove them to/from your team.
         </h1>
-        {/* <Grid>
-          <Grid.Column width={8}> */}
         <div style={{ flexDirection: 'row', display: 'flex' }}>
           <div style={{ flex: 1 }}>
-            <table>
-              <thead style={{ color: 'White' }}>
-                <tr>
-                  <th>Name</th>
-                  <th>Species</th>
-                  <th>Class</th>
-                  {/* <th>Ability</th> */}
-                  <th>Cost</th>
-                </tr>
-              </thead>
-
-              <tbody>
-                {heroes.map(({ name, species, className, cost }) => (
-                  <tr
-                    key={name}
-                    onClick={() => {
-                      this.handleHeroClick(name);
-                    }}
-                    style={{
-                      backgroundColor:
-                        pickedHeroes.indexOf(name) === -1
-                          ? 'Black'
-                          : 'DarkSlateGrey',
-                    }}
-                    // active={pickedHeroes.indexOf(name) !== -1}
-                  >
-                    <td
-                      style={{ color: colors.cost[cost], fontWeight: 'bold' }}
-                    >
-                      {name}
-                    </td>
-                    <td>
-                      {species.map(speciesName => (
-                        <React.Fragment key={speciesName}>
-                          <Feature name={speciesName} />{' '}
-                        </React.Fragment>
-                      ))}
-                    </td>
-                    <td>
-                      <Feature name={className} />
-                    </td>
-                    {/* <td> </td> */}
-                    <td style={{ color: 'White' }}>{cost}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-            {/* </Grid.Column>
-          <Grid.Column width={8}> */}
+            <HeroesTable
+              heroes={heroes.slice(0, 28)}
+              pickedHeroes={pickedHeroes}
+              onPickedHeroesChange={newPickedHeroes => {
+                this.setState({
+                  pickedHeroes: newPickedHeroes,
+                });
+              }}
+            />
+          </div>
+          <div style={{ flex: 1 }}>
+            <HeroesTable
+              heroes={heroes.slice(28, 55)}
+              pickedHeroes={pickedHeroes}
+              onPickedHeroesChange={newPickedHeroes => {
+                this.setState({
+                  pickedHeroes: newPickedHeroes,
+                });
+              }}
+            />
           </div>
           <div style={{ flex: 1 }}>
             <div style={{ color: 'White' }}>
@@ -109,15 +134,12 @@ export class HeroesList extends React.Component<{}, State> {
                         ({requiredCount}): {description}
                       </div>
                     ))}
-                    {/* <br /> */}
                   </div>
                 )
               )}
             </div>
           </div>
         </div>
-        {/* </Grid.Column>
-        </Grid> */}
       </div>
     );
   }
